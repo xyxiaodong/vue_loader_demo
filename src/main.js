972cfe83e73d44b6175d7271d11ceb2b3e311b1d
861794887@qq.com <39946424+xyxiaodong@users.noreply.github.com>
2018-07-29 20:56:33 +0800

    完成首页的动态数据渲染

--- src/main.js
+++ src/main.js
@@ -8,10 +8,36 @@ import App from './App.vue';
 import './assets/statics/site/css/style.css';
 //引入index组件
 import index from './components/index.vue';
+// 引入axios模块
+import axios from 'axios';
+//引入格式化时间插件moment.js
+import moment from 'moment';
+//引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//引入懒加载插件
+import VueLazyload from 'vue-lazyload';
+
+
+
+axios.defaults.baseURL = 'http://47.106.148.205:8899/';
+//设置全局过滤器
+Vue.filter('cutTime',function(value){
+   return moment(value).format('YYYY-MM-DD');
+});
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
+Vue.use(VueLazyload, {
+    // preLoad: 1.3,
+    error: require('./assets/logo.png'),
+    loading: require('./assets/logo.png'),
+    // attempt: 1
+  });
 //设置中间件
 Vue.use(VueRouter);
+// axios 把axios对象添加到Vue的原型上
+Vue.prototype.axios = axios;
 
 //设置路由
 const router = new VueRouter({
