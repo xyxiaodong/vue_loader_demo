46b107679d2375f7e1a65c3c43dd7a40c91961ac
861794887@qq.com <39946424+xyxiaodong@users.noreply.github.com>
2018-07-29 19:20:00 +0800

    完成首页静态页面布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+//引入vue模块
+import Vue from 'vue';
+//引入路由模块
+import VueRouter from 'vue-router';
+//引入App组件
+import App from './App.vue';
+//引入Css样式
+import './assets/statics/site/css/style.css';
+//引入index组件
+import index from './components/index.vue';
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+//设置中间件
+Vue.use(VueRouter);
 
+//设置路由
+const router = new VueRouter({
+    routes:[
+        {path:'/index',component:index},
+        {path:'/',redirect:'/index'}
+    ]
+})
+// 实例化vue对象
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
